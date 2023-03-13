8d2a1b874b7ff4fb4ab2c2d7d9a40e1779107903
Egearbox <pascal.moreno@gmail.com>
2023-03-13 07:57:52 +0100

    Changement Desc

--- client/src/components/Demo/Desc.jsx
+++ client/src/components/Demo/Desc.jsx
@@ -5,6 +5,7 @@ function Desc() {
         Take a look at <span className="code">client/src/contexts/EthContext</span>.
         This context maintains a global state and provides web3.js functionalities
         to the rest of the app.
+        apres moi je raconte ici une petite story histoire de faire un push GitHub
       </p>
       <p>
         Feel free to remove any component or styling that you don't need, and
